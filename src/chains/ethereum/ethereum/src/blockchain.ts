a596bff608dffddba6359e60a274df2ed4d6e018
soloseng <102702451+soloseng@users.noreply.github.com>
2023-02-22 15:45:24 -0500

    ++ precompiles to non-fork

--- src/chains/ethereum/ethereum/src/blockchain.ts
+++ src/chains/ethereum/ethereum/src/blockchain.ts
@@ -30,7 +30,10 @@ import {
   EVMResult,
   EVM
 } from "@ethereumjs/evm";
-import { EthereumInternalOptions, Hardfork } from "@soloseng/ganache-ethereum-options";
+import {
+  EthereumInternalOptions,
+  Hardfork
+} from "@soloseng/ganache-ethereum-options";
 import {
   Quantity,
   Data,
@@ -53,7 +56,11 @@ import {
   VmTransaction,
   TypedTransaction
 } from "@soloseng/ganache-ethereum-transaction";
-import { Block, RuntimeBlock, Snapshots } from "@soloseng/ganache-ethereum-block";
+import {
+  Block,
+  RuntimeBlock,
+  Snapshots
+} from "@soloseng/ganache-ethereum-block";
 import {
   SimulationTransaction,
   applySimulationOverrides,
@@ -736,20 +743,6 @@ export default class Blockchain extends Emittery<BlockchainTypedEvents> {
       // commit accounts, but for forking.
       const stateManager = <DefaultStateManager>this.vm.stateManager;
       await stateManager.checkpoint();
-
-      // XXX: <CELO> Pre-deploy registry contract at static address
-      const registryProxy = celoRegistryProxy(initialAccounts[0].address);
-      await stateManager.putContractCode(
-        registryProxy.address,
-        registryProxy.code
-      );
-      await stateManager.putContractStorage(
-        registryProxy.address,
-        registryProxy.storageKey,
-        registryProxy.storageValue
-      );
-      // </CELO>
-
       initialAccounts.forEach(account => {
         this.vm.eei.putAccount(account.address, account as any);
       });
@@ -795,59 +788,196 @@ export default class Blockchain extends Emittery<BlockchainTypedEvents> {
           block,
           blockLogs: BlockLogs.create(hash)
         }));
+    } else {
+      // commit accounts, but for forking.
+      const stateManager = <DefaultStateManager>this.vm.stateManager;
+      await stateManager.checkpoint();
+
+      // XXX: <CELO> Pre-deploy registry contract at static address
+      const registryProxy = celoRegistryProxy(initialAccounts[0].address);
+      await stateManager.putContractCode(
+        registryProxy.address,
+        registryProxy.code
+      );
+      await stateManager.putContractStorage(
+        registryProxy.address,
+        registryProxy.storageKey,
+        registryProxy.storageValue
+      );
+      // </CELO>
+
+      initialAccounts.forEach(account => {
+        this.vm.eei.putAccount(account.address, account as any);
+      });
+      await stateManager.commit();
+
+      const rawBlockNumber = Quantity.Empty;
+
+      // create the genesis block
+      const baseFeePerGas = this.common.isActivatedEIP(1559)
+        ? Block.INITIAL_BASE_FEE_PER_GAS
+        : undefined;
+
+      const genesis = new RuntimeBlock(
+        rawBlockNumber,
+        Data.from(BUFFER_32_ZERO),
+        this.coinbase,
+        blockGasLimit,
+        Quantity.Zero,
+        Quantity.from(timestamp),
+        this.isPostMerge ? Quantity.Zero : this.#options.miner.difficulty,
+        Quantity.Zero, // we start the totalDifficulty at 0
+        // we use the initial trie root as the genesis block's mixHash as it
+        // is deterministic based on initial wallet conditions
+        this.isPostMerge ? keccak(this.trie.root()) : BUFFER_32_ZERO,
+        baseFeePerGas
+      );
+
+      // store the genesis block in the database
+      const { block, serialized } = genesis.finalize(
+        KECCAK256_RLP,
+        KECCAK256_RLP,
+        BUFFER_256_ZERO,
+        this.trie.root(),
+        0n,
+        this.#options.miner.extraData,
+        [],
+        new Map()
+      );
+      block.header.number = Quantity.Zero;
+      const hash = block.hash();
+      return this.blocks
+        .putBlock(block.header.number.toBuffer(), hash, serialized)
+        .then(_ => ({
+          block,
+          blockLogs: BlockLogs.create(hash)
+        }));
     }
-
-    await this.#commitAccounts(initialAccounts);
-
-    // README: block `0` is weird in that a `0` _should_ be hashed as `[]`,
-    // instead of `[0]`, so we set it to `Quantity.Empty` instead of
-    // `Quantity.Zero` here. A few lines down in this function we swap
-    // this `Quantity.Empty` for `Quantity.Zero`. This is all so we don't
-    // have to have a "treat empty as 0` check in every function that uses the
-    // "latest" block (which this genesis block will be for brief moment).
-    const rawBlockNumber = Quantity.Empty;
-
-    // create the genesis block
-    const baseFeePerGas = this.common.isActivatedEIP(1559)
-      ? Block.INITIAL_BASE_FEE_PER_GAS
-      : undefined;
-
-    const genesis = new RuntimeBlock(
-      rawBlockNumber,
-      Data.from(BUFFER_32_ZERO),
-      this.coinbase,
-      blockGasLimit,
-      Quantity.Zero,
-      Quantity.from(timestamp),
-      this.isPostMerge ? Quantity.Zero : this.#options.miner.difficulty,
-      Quantity.Zero, // we start the totalDifficulty at 0
-      // we use the initial trie root as the genesis block's mixHash as it
-      // is deterministic based on initial wallet conditions
-      this.isPostMerge ? keccak(this.trie.root()) : BUFFER_32_ZERO,
-      baseFeePerGas
-    );
-
-    // store the genesis block in the database
-    const { block, serialized } = genesis.finalize(
-      KECCAK256_RLP,
-      KECCAK256_RLP,
-      BUFFER_256_ZERO,
-      this.trie.root(),
-      0n,
-      this.#options.miner.extraData,
-      [],
-      new Map()
-    );
-    // README: set the block number to an actual 0 now.
-    block.header.number = Quantity.Zero;
-    const hash = block.hash();
-    return this.blocks
-      .putBlock(block.header.number.toBuffer(), hash, serialized)
-      .then(_ => ({
-        block,
-        blockLogs: BlockLogs.create(hash)
-      }));
   };
+  // #initializeGenesisBlock = async (
+  //   timestamp: number,
+  //   blockGasLimit: Quantity,
+  //   initialAccounts: Account[]
+  // ) => {
+  //   if (this.fallback != null) {
+  //     const { block: fallbackBlock } = this.fallback;
+  //     const { miner: minerOptions } = this.#options;
+
+  //     // commit accounts, but for forking.
+  //     const stateManager = <DefaultStateManager>this.vm.stateManager;
+  //     await stateManager.checkpoint();
+
+  //     // XXX: <CELO> Pre-deploy registry contract at static address
+  //     const registryProxy = celoRegistryProxy(initialAccounts[0].address);
+  //     await stateManager.putContractCode(
+  //       registryProxy.address,
+  //       registryProxy.code
+  //     );
+  //     await stateManager.putContractStorage(
+  //       registryProxy.address,
+  //       registryProxy.storageKey,
+  //       registryProxy.storageValue
+  //     );
+  //     // </CELO>
+
+  //     initialAccounts.forEach(account => {
+  //       this.vm.eei.putAccount(account.address, account as any);
+  //     });
+  //     await stateManager.commit();
+
+  //     // create the genesis block
+  //     let baseFeePerGas: bigint;
+  //     if (this.common.isActivatedEIP(1559)) {
+  //       if (fallbackBlock.header.baseFeePerGas === undefined) {
+  //         baseFeePerGas = Block.INITIAL_BASE_FEE_PER_GAS;
+  //       } else {
+  //         baseFeePerGas = fallbackBlock.header.baseFeePerGas.toBigInt();
+  //       }
+  //     }
+  //     const genesis = new RuntimeBlock(
+  //       Quantity.from(fallbackBlock.header.number.toBigInt() + 1n),
+  //       fallbackBlock.hash(),
+  //       this.coinbase,
+  //       blockGasLimit,
+  //       Quantity.Zero,
+  //       Quantity.from(timestamp),
+  //       this.isPostMerge ? Quantity.Zero : minerOptions.difficulty,
+  //       fallbackBlock.header.totalDifficulty,
+  //       this.getMixHash(fallbackBlock.hash().toBuffer()),
+  //       baseFeePerGas
+  //     );
+
+  //     // store the genesis block in the database
+  //     const { block, serialized } = genesis.finalize(
+  //       KECCAK256_RLP,
+  //       KECCAK256_RLP,
+  //       BUFFER_256_ZERO,
+  //       this.trie.root(),
+  //       0n,
+  //       minerOptions.extraData,
+  //       [],
+  //       new Map()
+  //     );
+  //     const hash = block.hash();
+  //     return this.blocks
+  //       .putBlock(block.header.number.toBuffer(), hash, serialized)
+  //       .then(_ => ({
+  //         block,
+  //         blockLogs: BlockLogs.create(hash)
+  //       }));
+  //   }
+
+  //   await this.#commitAccounts(initialAccounts);
+
+  //   // README: block `0` is weird in that a `0` _should_ be hashed as `[]`,
+  //   // instead of `[0]`, so we set it to `Quantity.Empty` instead of
+  //   // `Quantity.Zero` here. A few lines down in this function we swap
+  //   // this `Quantity.Empty` for `Quantity.Zero`. This is all so we don't
+  //   // have to have a "treat empty as 0` check in every function that uses the
+  //   // "latest" block (which this genesis block will be for brief moment).
+  //   const rawBlockNumber = Quantity.Empty;
+
+  //   // create the genesis block
+  //   const baseFeePerGas = this.common.isActivatedEIP(1559)
+  //     ? Block.INITIAL_BASE_FEE_PER_GAS
+  //     : undefined;
+
+  //   const genesis = new RuntimeBlock(
+  //     rawBlockNumber,
+  //     Data.from(BUFFER_32_ZERO),
+  //     this.coinbase,
+  //     blockGasLimit,
+  //     Quantity.Zero,
+  //     Quantity.from(timestamp),
+  //     this.isPostMerge ? Quantity.Zero : this.#options.miner.difficulty,
+  //     Quantity.Zero, // we start the totalDifficulty at 0
+  //     // we use the initial trie root as the genesis block's mixHash as it
+  //     // is deterministic based on initial wallet conditions
+  //     this.isPostMerge ? keccak(this.trie.root()) : BUFFER_32_ZERO,
+  //     baseFeePerGas
+  //   );
+
+  //   // store the genesis block in the database
+  //   const { block, serialized } = genesis.finalize(
+  //     KECCAK256_RLP,
+  //     KECCAK256_RLP,
+  //     BUFFER_256_ZERO,
+  //     this.trie.root(),
+  //     0n,
+  //     this.#options.miner.extraData,
+  //     [],
+  //     new Map()
+  //   );
+  //   // README: set the block number to an actual 0 now.
+  //   block.header.number = Quantity.Zero;
+  //   const hash = block.hash();
+  //   return this.blocks
+  //     .putBlock(block.header.number.toBuffer(), hash, serialized)
+  //     .then(_ => ({
+  //       block,
+  //       blockLogs: BlockLogs.create(hash)
+  //     }));
+  // };
 
   /**
    * The number of milliseconds time should be adjusted by when computing the
